28ba5aef40f374ac762b3e8412fae291748fcf1d
Patrick Kong <kongpatrick97@gmail.com>
2019-06-03 13:37:34 -0700

    Added flag spawning code

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using HTC.UnityPlugin.Vive;
+
+public class Flag : MonoBehaviour {
+
+    Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+        newPosition = transform.position;
+        GetComponent<Renderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Renderer>().enabled = true;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = hit.point;
+                transform.position = newPosition;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+	}
+}
