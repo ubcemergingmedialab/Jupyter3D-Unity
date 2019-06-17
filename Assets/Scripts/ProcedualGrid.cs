2662a03e8ccd7544ad8853411b9ec97cc6648d00
Rayhan Fakim <rayhan.fakim@gmail.com>
2019-06-17 10:18:19 -0700

    Added new Demo
    From Procedural Tutorial then implemented different funcitons. Added new "Procedural" Script with new "Grid Demo" scene while using "GraphFunction" and "GraphFunctionName" to have the list of different available functions.

---
+++ Assets/Scripts/ProcedualGrid.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+
+public class ProcedualGrid : MonoBehaviour {
+
+    // initializing lists and the mesh
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    
+    // grid setting
+    public float cellSize = 1;
+    public Vector3 gridOffset; // to change the initial x y z position pf the grid
+    public int gridSize; // Square grid, else create two variables 
+
+    [Range(0, 1)] // discrete or continuous
+    public int meshImplementation = 0;
+
+    // functions list 
+    public GraphFunctionName function;
+    static GraphFunction[] functions = { //Array of all the methods/functions to graph that are available to be used
+        SineFunction, Sine2DFunction1, Sine2DFunction2, MultiSineFunction, MultiSine2DFunction,
+        Cone, Ripple, RippleFading, RippleDynamic
+    };
+
+    const float pi = Mathf.PI;
+
+
+    // Use this for initialization
+    void Awake () {
+
+        mesh = GetComponent<MeshFilter>().mesh;
+		
+	}
+	
+	void Update () {
+
+        
+
+        if (meshImplementation  == 0)
+        {
+            MakeDiscreteProceduralGrid();
+
+        } else if (meshImplementation == 1)
+        {
+            MakeContiguousProceduralGrid();
+        }
+        UpdateMesh();
+		
+	}
+
+    void MakeDiscreteProceduralGrid()
+        // Populating the informations, creating these arrays and filling it with the appropriate information 
+    {
+        GraphFunction f = functions[(int) function]; // Method delegation part using the array of functions defined above
+        float sec = Time.time;        // Variable t refers to time 
+
+
+        // set array sizes
+        vertices = new Vector3[gridSize * gridSize * 4];  // only 4 vertices - GridSize * Gridsize, to make more and in two dimensions
+        triangles = new int[gridSize * gridSize * 6];     // 6 sides for 2 triangles to make a quad - so not sharing any vertices, all distinct mesh 
+
+        // set tracker intergers
+        int v = 0;  // tracker for vertices 
+        int t = 0;  // tracker for triangles
+
+        // set vertex offset - because we don't have a permanent size 
+        float vertexOffSet = cellSize * 0.5f; // deviding is more expensive than multiplying - to do this have in the middle at the origin  - shifter at the origin rather than being in a corner
+
+        for (int x = 0; x < gridSize; x++) // iterating through the x dimension 
+        {
+            for (int z = 0; z < gridSize; z++) // iterating through the z dimension
+            {
+                Vector3 cellOffSet = new Vector3 (x * cellSize, f(x,z,sec) , z * cellSize) ; // to not have all cell stack on top of each other, change one cell offset - using x and y two know exactly which cell we are at 
+
+                // populate the vertices and triangles arrays 
+                // addind the tracker to keep going thats is 0123 then 4567 then ... in order to iterate through all vertices of the grid
+                vertices[0 + v] = new Vector3(-vertexOffSet, 0, -vertexOffSet) + gridOffset + cellOffSet;
+                vertices[1 + v] = new Vector3(-vertexOffSet, 0, vertexOffSet)  + gridOffset + cellOffSet;
+                vertices[2 + v] = new Vector3(vertexOffSet, 0, -vertexOffSet)  + gridOffset + cellOffSet;
+                vertices[3 + v] = new Vector3(vertexOffSet, 0, vertexOffSet)   + gridOffset + cellOffSet;
+
+                // hard coding the pattern as it is always the same - two triangles, sharing two vertices, different order to keep it as clockwise
+                // associting the triangle with the current vertices - that is + v (tracker)
+                triangles[0 + t] = 0 + v;
+                triangles[1 + t] = triangles[4 + t] = 1 + v;
+                triangles[2 + t] = triangles[3 + t] = 2 + v;
+                triangles[5 + t] = 3 + v;
+
+                v += 4;
+                t += 6;
+            }
+        }
+
+    }
+
+    void MakeContiguousProceduralGrid()
+    // Populating the informations, creating these arrays and filling it with the appropriate information 
+    {
+        GraphFunction f = functions[(int)function]; // Method delegation part using the array of functions defined above
+        float sec = Time.time;        // Variable t refers to time 
+
+        // set array sizes
+        vertices = new Vector3[(gridSize + 1) * (gridSize + 1)];  // girdsize two dimentions, + plus one to have that final corner 
+        triangles = new int[gridSize * gridSize * 6];     // 6 sides for 2 triangles to make a quad 
+        
+        // set tracker intergers
+        int v = 0;  // tracker for vertices 
+        int t = 0;  // tracker for triangles
+        ;
+        // set vertex offset - because we don't have a permanent size 
+        float vertexOffSet = cellSize * 0.5f; // deviding is more expensive than multiplying - to do this have in the middle at the origin  - shifter at the origin rather than being in a corner
+
+
+        // Setting the vertices 
+        for (int x = 0; x <= gridSize; x++) // iterating through the x dimension - less or equal because we added the + 1
+        {
+            for (int z = 0; z <= gridSize; z++) // iterating through the z dimension - less or equal because we added the + 1
+            {
+                vertices[v] = new Vector3 ((x * cellSize) - vertexOffSet, f(x, z, sec), (z * cellSize) - vertexOffSet);  // starting off at half of the cell and shifting it 
+                v++;
+            }
+        }
+
+        v = 0; // reset vertex tracker, as it got incresed in the previous for loop
+
+        // Setting each cell's triangles
+        for (int x = 0; x < gridSize; x++) // iterating through the x dimension - no need the equal side cause no + 1
+        {
+            for (int z = 0; z < gridSize; z++) // iterating through the z dimension - no need the equal side cause no + 1
+            {
+                // first triangle, using the origin point 
+                // second, to the right of the first
+                // third, up one row of the first 
+                // fourth, up one row but into the right space of the first 
+
+                triangles[0 + t] = 0 + v; 
+                triangles[1 + t] = triangles[4 + t] = 1 + v;
+                triangles[2 + t] = triangles[3 + t] = (gridSize + 1) + v; // same position as one but up one row
+                triangles[5 + t] = (gridSize + 1) + v + 1; // up right of the first 
+
+                v++;
+                t += 6; //6 because of the triangle situation 
+            }
+            v++; // iterate our vertices one more time so that we start at a new row 
+        }
+    }
+
+    
+
+    void UpdateMesh()
+    {
+        mesh.Clear(); // clearing the mesh to make sure there is no existing information 
+
+        mesh.vertices = vertices;   // assigning our vertices 
+        mesh.triangles = triangles; // assigning our triangles 
+
+        mesh.RecalculateNormals(); // fixing the lightening issue with the new normals
+    }
+
+
+
+
+    static float SineFunction(float x, float z, float t)        // float function because it needs to return a value 
+    {
+        return Mathf.Sin(pi * (x + t));
+    }
+
+    static float MultiSineFunction(float x, float z, float t) // static becayse it is not associated with any specific object or vlaue instances
+    {
+        float y = Mathf.Sin(pi * (x + t));
+        y += Mathf.Sin(2f * pi * (x + 2f * t)) / 2f;       // adding complexity 
+        y *= 2f / 3f;
+        return y;
+    }
+
+    static float Sine2DFunction1(float x, float z, float t)
+    {
+        return Mathf.Sin(pi * (x + z + t));
+    }
+
+    static float Sine2DFunction2(float x, float z, float t)
+    {
+        float y = Mathf.Sin(pi * (x + t));
+        y += Mathf.Sin(pi * (z + t));
+        y *= 0.5f;
+        return y;
+    }
+
+    static float MultiSine2DFunction(float x, float z, float t)
+    {
+        float y = 4f * Mathf.Sin(pi * (x + z + t * 0.5f));
+        y += Mathf.Sin(pi * (x + t));
+        y += Mathf.Sin(2f * pi * (z + 2f * t)) * 0.5f;
+        y *= 1f / 5.5f;
+        return y;
+
+    }
+
+    static float Cone(float x, float z, float t)
+    {
+        float d = Mathf.Sqrt(x * x + z * z);
+        float y = d;
+        return y;
+    }
+
+    static float Ripple(float x, float z, float t)
+    {
+        float d = Mathf.Sqrt(x * x + z * z);
+        float y = Mathf.Sin(4f * pi * d);
+        return y;
+    }
+
+    static float RippleFading(float x, float z, float t)
+    {
+        float d = Mathf.Sqrt(x * x + z * z);
+        float y = Mathf.Sin(4f * pi * d);
+        y /= 1f + 10f * d;
+        return y;
+    }
+
+    // Mexican Hat
+    // Mathematical implementation
+    static float RippleDynamic(float x, float z, float t)
+    {
+        float d = Mathf.Sqrt(x * x + z * z);
+        float y = Mathf.Sin(pi * (4f * d - t));
+        y /= 1f + 10f * d;
+        return y;
+    }
+}
