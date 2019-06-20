38693af5c159220fee5cf6d1adcd45d392af1349
pkong97 <kongpatrick97@gmail.com>
2019-06-19 18:37:04 -0700

    updated Flag.cs

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -37,43 +36,4 @@ public class Flag : MonoBehaviour {
         }
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
-using HTC.UnityPlugin.Vive;
 
-public class Flag : MonoBehaviour {
-
-    Vector3 newPosition;
-
-	// Use this for initialization
-	void Start () {
-        newPosition = transform.position;
-        GetComponent<Renderer>().enabled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            GetComponent<Renderer>().enabled = true;
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                newPosition = hit.point;
-                transform.position = newPosition;
-            }
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            GetComponent<Renderer>().enabled = false;
-        }
-	}
-}
->>>>>>> 3e793d5f20e3c128cb4490d68f3ebedc3ae713e0
