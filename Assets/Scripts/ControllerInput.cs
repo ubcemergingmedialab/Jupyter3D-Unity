2f21351809782ea24c78ba5eba610b50d872cf10
Rayhan Fakim <rayhan.fakim@gmail.com>
2019-06-24 16:17:53 -0700

    Changed controllers functions

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -5,16 +5,18 @@ using HTC.UnityPlugin.Vive;
 
 public class ControllerInput : MonoBehaviour {
 
-    public float BaseSpeed = 5f;
-    public float BaseAmp = 4;
+    public float BaseSpeed = 10f;
+    public float BaseAmp = 0;
+    //public float BaseK = ((2f * pi) / 2);
+
     //public float AmpIncrease = 0.25f;
     private void OnEnable()
     {
-        ViveInput.AddListenerEx(HandRole.RightHand, ControllerButton.Trigger, ButtonEventType.Down, OnTrigger);
+        ViveInput.AddListenerEx(HandRole.LeftHand, ControllerButton.Trigger, ButtonEventType.Down, OnTrigger);
     }
     private void OnDisable()
     {
-        ViveInput.RemoveListenerEx(HandRole.RightHand, ControllerButton.Trigger, ButtonEventType.Down, OnTrigger);
+        ViveInput.RemoveListenerEx(HandRole.LeftHand, ControllerButton.Trigger, ButtonEventType.Down, OnTrigger);
     }
 
     private void OnTrigger()
@@ -24,21 +26,45 @@ public class ControllerInput : MonoBehaviour {
 
     private void Update()
     {
+        triggerFunctionality();
+        joystickYFunctionality();
 
-        ProcedualGrid.speed = ViveInput.GetAxisEx(HandRole.RightHand, ControllerAxis.Trigger) * BaseSpeed;
-        Debug.Log("Changing speed to " + ProcedualGrid.speed);
-
-        //if (ViveInput.GetAxisEx(HandRole.RightHand, ControllerAxis.Trigger) > 0.5f)
-        //{
-        //    ProcedualGrid.amplitude += AmpIncrease;
-        //}
-
-        //if (ViveInput.GetAxisEx(HandRole.RightHand, ControllerAxis.Trigger) < -0.5f)
-        //{
-        //    ProcedualGrid.amplitude -= AmpIncrease;
-        //}
-
-
-        ProcedualGrid.amplitude = ViveInput.GetAxisEx(HandRole.RightHand, ControllerAxis.JoystickY) * BaseAmp  + 1;
     }
+
+    private void triggerFunctionality()
+    {
+        float trigger = (ViveInput.GetAxisEx(HandRole.LeftHand, ControllerAxis.Trigger));
+        
+        
+        if (trigger >= 0.5f && trigger > 0.0f)
+        {
+            ProcedualGrid.speed = BaseSpeed;
+        }
+        if (trigger< 0.5f && trigger> 0.0f)
+        {
+            ProcedualGrid.speed = BaseSpeed* 0.5f;
+        }
+        if (trigger == 0.0f)
+        {
+            ProcedualGrid.speed = 0;
+        }
+    }
+
+    private void joystickYFunctionality()
+    {
+
+        float joyStickY = ViveInput.GetAxisEx(HandRole.LeftHand, ControllerAxis.JoystickY);
+
+        if (joyStickY > 0.90f)
+        {
+            ProcedualGrid.amplitude += BaseAmp;
+        }
+        if (joyStickY < -0.90f)
+        {
+            ProcedualGrid.amplitude -= BaseAmp;
+        }
+        // ProcedualGrid.amplitude = ViveInput.GetAxisEx(HandRole.LeftHand, ControllerAxis.JoystickY) * BaseAmp + 1;
+    }
+
+
 }
