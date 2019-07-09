51304b93ed6b32d4e1cc345a9812cfb9efd414f2
Amelia He <amelia.he98@gmail.com>
2019-07-09 14:35:41 -0700

    Update ControllerInput.cs

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using HTC.UnityPlugin.Vive;
 
+//tester
 public class ControllerInput : MonoBehaviour
 {
     CharacterController characterController;
