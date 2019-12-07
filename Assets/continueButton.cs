22335a7f930dc7b0021ef9820432832a4aa9a3f2
Kyle Mas <kyle.mas567@gmail.com>
2019-12-06 16:22:51 -0800

    Added button to allow switching between menu panels on welcome scene, added button script.

---
+++ Assets/continueButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class continueButton : MonoBehaviour {
+    public int menuID = 0;
+    public GameObject[] menuPanels;
+    private GameObject introductionPanel;
+    private GameObject tutorialOnePanel;
+
+	// Use this for initialization
+	void Start () {
+   
+        introductionPanel = GameObject.Find("IntroductionPanel");
+        menuPanels[0] = introductionPanel;
+        tutorialOnePanel = GameObject.Find("TutorialOnePanel");
+        menuPanels[1] = tutorialOnePanel;
+        switchToMenu(menuID);
+    }
+
+    public void introToOne() {
+        switchToMenu(1);
+    }
+
+    public void switchToMenu(int menuID) {
+        foreach (GameObject panel in menuPanels) {
+            if (panel != introductionPanel)
+            {
+                panel.gameObject.SetActive(false);
+            }
+        }
+    
+        switch (menuID) {
+            case 0:
+                introductionPanel.gameObject.SetActive(true);
+                break;
+
+            case 1:
+                introductionPanel.gameObject.SetActive(false);
+                tutorialOnePanel.gameObject.SetActive(true);
+                break;
+      }
+   }
+}
