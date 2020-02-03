c19583062738a7fc170ef25cee8cfae1b1cd1f4f
Kyle Mas <34122775+kylemas@users.noreply.github.com>
2020-02-03 15:15:06 -0800

    New Welcome Scene (#60)
    * Added panel and VROrigin onto welcome scene.
    
    * Removed elements on menu panel,
    
    * Added button & text for welcome panel.
    
    * Added button to allow switching between menu panels on welcome scene, added button script.
    
    * Added introduction text to welcome scene.
    
    * Added menu panels for instructions and diagram assets.
    
    * Added new buttons in welcome scene, fixed buttonScript
    
    * Fixed diagram images, added labels for panels, readjustments
    
    * Fixed layout for welcome scene introductionPanel
    
    * Fixed scripts, buttons are working to switch between panels
    
    * Update welcomeScene.unity

---
+++ Assets/Scripts/continueButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class continueButton : MonoBehaviour {
+    public GameObject[] menuPanels;
+    public GameObject introductionPanel;
+    
+
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    public void introToOne() {
+        switchPanel(1);
+    }
+
+   
+
+    public void switchPanel(int menuID) {
+        switch (menuID)
+        {
+            case 1:
+                menuPanels[1].gameObject.SetActive(true);
+                introductionPanel.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+
+}
